e6bb4881a160e100fe380ec0ac0e5870731915c5
Roman Oryol <roman.oryol83@gmail.com>
2025-04-22 11:11:43 +0300

    Добавить валидацию для выбора страны и почтового кода в форме

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('form');
 const emailField = form.querySelector('input[name="email"]');
+const countrySelect = form.querySelector('select');
+const postalCodeField = form.querySelector('input[name="postal-code"]');
 
 const isEmailValid = function (email) {
   const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -23,9 +25,62 @@ const checkEmail = function () {
   }
 };
 
+const checkCountry = function () {
+  const errorEl = form.querySelector('select + .error');
+  const invalidSelectMessage = 'Пожалуйста, выберите страну';
+
+  if (countrySelect.value === '') {
+    errorEl.textContent = invalidSelectMessage;
+    countrySelect.setCustomValidity(invalidSelectMessage);
+  } else {
+    errorEl.textContent = '';
+    countrySelect.setCustomValidity('');
+  }
+};
+
+const checkPostalCode = function () {
+  const postalCodeRules = {
+    '': [/^.*$/, 'Пожалуйста, выберите страну'],
+    BY: [/^\d{6}$/, 'Пример кода выбранной страны: 220030'],
+    HU: [/^\d{4}$/, 'Пример кода выбранной страны: 1234'],
+    RU: [/^\d{6}$/, 'Пример кода выбранной страны: 123456'],
+    RS: [/^\d{5}$/, 'Пример кода выбранной страны: 11000'],
+    UA: [/^\d{5}$/, 'Пример кода выбранной страны: 01001'],
+  };
+  const errorEl = form.querySelector('input[name="postal-code"] + .error');
+  const country = countrySelect.value.trim();
+  const postalCode = postalCodeField.value;
+
+  if (country === '') {
+    postalCodeField.setCustomValidity('');
+  }
+
+  if (postalCodeRules[country][0].test(postalCode)) {
+    postalCodeField.setCustomValidity('');
+    errorEl.textContent = '';
+  } else {
+    postalCodeField.setCustomValidity(postalCodeRules[country][1]);
+    errorEl.textContent = postalCodeField.validationMessage;
+  }
+};
+
 emailField.addEventListener('blur', () => {
   emailField.classList.add('touched');
   checkEmail();
 });
 
 emailField.addEventListener('input', checkEmail);
+
+countrySelect.addEventListener('blur', () => {
+  countrySelect.classList.add('touched');
+  checkCountry();
+});
+
+countrySelect.addEventListener('change', checkCountry);
+
+postalCodeField.addEventListener('blur', () => {
+  postalCodeField.classList.add('touched');
+  checkPostalCode();
+});
+
+postalCodeField.addEventListener('input', checkPostalCode);
